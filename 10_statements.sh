5787203f1dc8c28806894a7aeea50b43efa1f4df
T.S. Hayagriva Kishan <sharmilakishan.2003@gmail.com>
2023-10-14 21:25:47 +0530

    Conditional Statements

---
+++ 10_statements.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+# Conditional Statents
+
+read -p "Enter your marks : " marks
+
+if [[ $marks -gt 40 ]]
+then
+	echo "You are PASS"
+else 
+	echo "You are FAIL!!!!!!!"
+fi
---
+++ 11_elif.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+# Conditional Statents elif
+
+read -p "Enter your marks : " marks
+
+if [[ $marks -ge 80 ]]
+then 
+	echo "First Divion"
+elif [[ $marks -ge 60 ]] 
+then
+	echo "Second Division"
+elif [[ $marks -ge 40 ]]
+then 
+	echo "Third Division"
+else 
+	echo "You are FAIL!!!!!!!"
+fi
