9a2c030b8e040427f81633edd35c36ff7d29b052
T.S. Hayagriva Kishan <sharmilakishan.2003@gmail.com>
2023-10-21 19:21:00 +0530

    functions repo

---
+++ 24_functions_demo.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# To make a function
+function welcomeNote {
+     echo " - - - - - - - - - "
+     echo "Welcome"
+     echo " - - - - - - - - - "
+}
+
+# To call our fucntion
+welcomeNote
+welcomeNote
+welcomeNote
+
+welcomeFun(){
+     echo " - - - - - - - - - "
+     echo "Welcome"
+     echo " - - - - - - - - - "
+}
+#To call our fucntion
+welcomeNote
+welcomeNote
