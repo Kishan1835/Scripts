8cb481d71d864240ffd6ba09927bfd64a46d63c8
T.S. Hayagriva Kishan <sharmilakishan.2003@gmail.com>
2023-10-21 19:21:21 +0530

    functions wuth arguments

---
+++ 25_function_with_arg.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+function welcomeNode {
+	echo "- - - - - - - - - -"
+	echo "Welcome $1"
+	echo "Age is $2"
+	echo "- - - - - - - - - - "
+}
+
+welcomeNode kishan 12
+welcomeNode raju 34
